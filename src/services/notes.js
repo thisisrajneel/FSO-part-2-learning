c946cb8413451b54dd439e1369a8c935caacf39d
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2022-03-14 17:05:43 +0530

    changed baseurl to express backend

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseurl = 'http://localhost:3001/notes'
+const baseurl = 'http://localhost:3001/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseurl)
